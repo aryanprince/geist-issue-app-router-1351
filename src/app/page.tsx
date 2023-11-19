9f37d6f3402cf66d404c45e4b687ea1bb9e7f59a
Aryan Prince <aryanprincesanty@icloud.com>
2023-11-19 04:14:27 +0000

    Added padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-    <>
+    <div className="p-4">
       <h1 className="text-4xl font-semibold tracking-tight">
         this text uses geist font, works fine on app router
       </h1>
       <p className="text-2xl font-semibold text-neutral-400">
         ver. geist@1.1.0
       </p>
-    </>
+    </div>
   )
 }
